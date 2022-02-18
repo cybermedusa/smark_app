3a81e6d861cc740e4661993ac80c9c533b1d3fbb
zuzka98 <zuzannakilar@gmail.com>
2022-02-18 13:25:47 +0100

    SMARK APP correction of values

--- smark_app.py
+++ smark_app.py
@@ -1,41 +1,37 @@
 import pandas as pd
 from sqlalchemy import create_engine
-import age_gender_distribution_stacked_bar, avg_max_min_pln_per_city_district
-import parking_time_before_after_app, parking_time_distribution_sub_non_sub_comparison
-import rate_heart, subscription_type_pie, users_registration_distribution
-import users_parking_distribution_per_month_bar, profits_costs_distribution_per_months
 
 pd.options.mode.chained_assignment = None
 
 # Connection to the database
-db_connection_str = 'mysql+pymysql://root:' + "******" + "@localhost:3306/smark"
+db_connection_str = 'mysql+pymysql://root:' + "*********" + "@localhost:3306/smark"
 db_connection = create_engine(db_connection_str)
 
 # Age & gender distribution of all users
-print(age_gender_distribution_stacked_bar)
+# age_gender_distribution_stacked_bar.py
 
 # Subscription & non-subscription users distribution
-print(subscription_type_pie)
+# subscription_type_pie.py
 
 # Registration distribution per months in 2021
-print(users_registration_distribution)
+# users_registration_distribution
 
 # Users parking distribution per months
-print(users_parking_distribution_per_month_bar)
+# users_parking_distribution_per_month_bar.py
 
 # Users average parking time distribution per city district
-print(parking_time_distribution_sub_non_sub_comparison)
+# parking_time_distribution_sub_non_sub_comparison
 
 # Avg, max, min parking cost per city district
-print(avg_max_min_pln_per_city_district)
+# avg_max_min_pln_per_city_district
 
 # Time needed to find parking spot before and after app
-print(parking_time_before_after_app)
+# parking_time_before_after_app.py
 
 # Profits and costs distribution per months in 2021
-print(profits_costs_distribution_per_months)
+# profits_costs_distribution_per_month.py
 
 # Average app rate
-print(rate_heart)
+# rate_heart.py
 
 
--- users_registration_distribution.py
+++ users_registration_distribution.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from smark_app import users_df
+from subscription_type_pie import users_df
 
 users_df['created_at'] = pd.to_datetime(users_df['created_at'])
 grouped_users_df = users_df.groupby(pd.Grouper(key='created_at', freq='M'))['id'].count()
